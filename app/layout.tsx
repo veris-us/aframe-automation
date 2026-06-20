f58c9a1a22b3de4802fe917e3556c20280b47e1b
Michael McDonald <mgmcdonald9038@gmail.com>
2026-06-20 17:13:57 -0500

    Add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
 variable: "--font-geist-sans",
@@ -48,9 +49,10 @@ children,
 children: React.ReactNode;
 }>) {
 return ( <html lang="en">
-<body
-className={`${geistSans.variable} ${geistMono.variable} antialiased`}
->
-{children} </body> </html>
+<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+  {children}
+  <Analytics />
+</body>
+</html>
 );
 }
